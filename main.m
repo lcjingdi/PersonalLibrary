1900f9d2dc2ae8ce7300a2802fed2816376df588
lcjingdi <lcjingdi@gmail.com>
2015-09-28 15:19:01 +0800

    firstInit
    firstInit

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PersonalLibrary
+//
+//  Created by lcjingdi on 15/9/28.
+//  Copyright © 2015年 lcjingdi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
